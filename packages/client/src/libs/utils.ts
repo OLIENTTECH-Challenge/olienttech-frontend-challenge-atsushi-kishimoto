6be891748dd8e4df091819007dc4cb3c3635fcb5
Akkun <akkunlab@gmail.com>
2024-02-27 17:24:00 +0900

    Add: formatDate function

--- packages/client/src/libs/utils.ts
+++ packages/client/src/libs/utils.ts
@@ -5,3 +5,15 @@ export const classNames = (...classes: (string | undefined)[]) => {
 export const formatMoney = (amount: number): string => {
   return amount.toLocaleString();
 };
+
+export const formatDate = (isoString: string): string => {
+  const date = new Date(isoString);
+  return new Intl.DateTimeFormat('ja-JP', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  }).format(date);
+};
